5a853b8749e0fcc6869a064f5ed259e3d1f64005
Christophe '116' Loiseau <116@lab0.net>
2021-03-18 18:34:29 +0100

    Wood3 submission

--- app/src/main/kotlin/net/lab0/coderoyale/Ai.kt
+++ app/src/main/kotlin/net/lab0/coderoyale/Ai.kt
@@ -1,4 +1,5 @@
 import java.util.*
+import kotlin.math.sqrt
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -9,19 +10,79 @@ fun debug(any: Any?) {
   System.err.println(any.toString())
 }
 
-inline class TouchedSite(val touchedSite: Int) {
+// constants
+
+// sites
+
+const val EMPTY_STRUCTURE_TYPE = -1
+const val BARRACKS_STRUCTURE_TYPE = 2
+
+const val NO_OWNER = -1
+const val ENEMY_OWNER = 1
+
+// units
+
+const val FRIENDLY_OWNER = 0
+
+const val QUEEN_TYPE = -1
+const val KNIGHT_TYPE = 0
+const val ARCHER_TYPE = 1
+
+const val KNIGHT_COST = 80
+const val ARCHER_COST = 100
+
+inline class TouchedSite(val siteId: Int) {
   val touches: Boolean
-    get() = touchedSite != -1
+    get() = siteId != -1
 }
 
-data class BuildingGround(val siteId: Int, val x: Int, val y: Int, val radius: Int)
+data class MapSite(val siteId: Int, val position: Position, val radius: Int)
 
 data class Battlefield(
+  val mapSites: List<MapSite>,
   val gold: Int,
   val touchedSite: TouchedSite,
   val sites: List<Site>,
   val units: List<Soldier>
-)
+) {
+  // gold
+
+  val canBuildKnight: Boolean
+    get() = gold >= KNIGHT_COST
+
+  val canBuildArcher: Boolean
+    get() = gold >= ARCHER_COST
+
+
+  // sites
+
+  val mySites: List<Site>
+    get() = sites.filter { it.isFriendly }
+
+  val iHaveBarracks: Boolean
+    get() = myBarracks.isNotEmpty()
+
+  val myBarracks: List<Site>
+    get() = mySites.filter { it.isBarracks }
+
+  val touchedSiteAsSite: Site
+    get() = sites.first { it.siteId == touchedSite.siteId }
+
+  /**
+   * Sites that noone owns. Lazy
+   */
+  val emptySites: List<MapSite> by lazy {
+    val emptySites = sites.filter { it.isEmpty }.map { it.siteId }
+    mapSites.filter {
+      it.siteId in emptySites
+    }
+  }
+
+  // units
+
+  val queen: Soldier
+    get() = units.first { it.isQueen }
+}
 
 data class Site(
   val siteId: Int,
@@ -33,33 +94,37 @@ data class Site(
   val param2: Int
 ) {
   val isBarracks: Boolean
-    get() = structureType == 2
+    get() = structureType == BARRACKS_STRUCTURE_TYPE
 
   val isEmpty: Boolean
-    get() = structureType == -1
+    get() {
+      return structureType == EMPTY_STRUCTURE_TYPE
+    }
 
   val isFriendly: Boolean
-    get() = owner == 0
+    get() = owner == FRIENDLY_OWNER
 
   val isEnemy: Boolean
-    get() = owner == 1
+    get() = owner == ENEMY_OWNER
+
+  val isNotOwned: Boolean
+    get() = owner == NO_OWNER
 }
 
 data class Soldier(
-  val x: Int,
-  val y: Int,
+  val position: Position,
   val owner: Int,
   val unitType: Int,
   val health: Int
 ) {
   val isQueen: Boolean
-    get() = unitType == -1
+    get() = unitType == QUEEN_TYPE
 
   val isKnight: Boolean
-    get() = unitType == 0
+    get() = unitType == KNIGHT_TYPE
 
   val isArcher: Boolean
-    get() = unitType == 1
+    get() = unitType == ARCHER_TYPE
 }
 
 sealed class Action(val command: String) {
@@ -68,21 +133,35 @@ sealed class Action(val command: String) {
     constructor(x: Int, y: Int) : this(Position(x, y))
   }
 
-  data class BuildKnightBarracks(val siteId: Int) : Action("BUILD $siteId BARRACKS-KNIGHT")
+  data class BuildKnightBarracks(val siteId: Int) :
+    Action("BUILD $siteId BARRACKS-KNIGHT")
+
   data class BuildArcherBarracks(val siteId: Int) : Action("BUILD $siteId BARRACKS-ARCHER")
 }
 
-data class Position(val x: Int, val y: Int)
+data class Position(val x: Int, val y: Int) {
+  // euclidean distance, ignore obstacles
+  fun distanceTo(other: Position): Double {
+    val xDiff = other.x - this.x
+    val yDiff = other.y - this.y
+    val xSqr = xDiff * xDiff
+    val ySqr = yDiff * yDiff
 
-sealed class TrainSoldiers(val command: String) {
-  object None : TrainSoldiers("TRAIN")
+    // not float as it's double internally
+    // and x86 uses 80 bits floating point anyway
+    return sqrt((xSqr + ySqr).toDouble())
+  }
+}
+
+sealed class Training(val command: String) {
+  object None : Training("TRAIN")
   class AtLocation(locationIds: List<Int>) :
-    TrainSoldiers("TRAIN " + locationIds.joinToString(" ") { it.toString() }) {
+    Training("TRAIN " + locationIds.joinToString(" ") { it.toString() }) {
     constructor(vararg locationIds: Int) : this(locationIds.toList())
   }
 }
 
-fun playTurn(action: Action, train: TrainSoldiers): List<String> {
+fun playTurn(action: Action, train: Training): List<String> {
   val out = listOf(action.command, train.command)
 
   out.forEach { println(it) }
@@ -95,13 +174,13 @@ fun main(args: Array<String>) {
   val input = Scanner(System.`in`)
   val numSites = input.nextInt()
 
-  val sites = (0 until numSites).map {
+  val existingSites = (0 until numSites).map {
     val siteId = input.nextInt()
     val x = input.nextInt()
     val y = input.nextInt()
     val radius = input.nextInt()
 
-    BuildingGround(siteId, x, y, radius)
+    MapSite(siteId, Position(x, y), radius)
   }
 
   val turns = mutableListOf<Battlefield>()
@@ -111,36 +190,82 @@ fun main(args: Array<String>) {
     val gold = input.nextInt()
     val touchedSite = input.nextInt() // -1 if none
 
-    val sites = (0 until numSites).map {
-      val siteId = input.nextInt()
-      val ignore1 = input.nextInt() // used in future leagues
-      val ignore2 = input.nextInt() // used in future leagues
-      val structureType = input.nextInt() // -1 = No structure, 2 = Barracks
-      val owner = input.nextInt() // -1 = No structure, 0 = Friendly, 1 = Enemy
-      val param1 = input.nextInt()
-      val param2 = input.nextInt()
-
-      Site(siteId, ignore1, ignore2, structureType, owner, param1, param2)
-    }
-
+    val sites = parseSites(numSites, input)
     val numUnits = input.nextInt()
-    val units = (0 until numUnits).map {
-      val x = input.nextInt()
-      val y = input.nextInt()
-      val owner = input.nextInt()
-      val unitType = input.nextInt() // -1 = QUEEN, 0 = KNIGHT, 1 = ARCHER
-      val health = input.nextInt()
+    val units = parseUnits(numUnits, input)
 
-      Soldier(x, y, owner, unitType, health)
-    }
-
-
-    val battlefield = Battlefield(gold, TouchedSite(touchedSite), sites, units)
-
+    val battlefield = Battlefield(existingSites, gold, TouchedSite(touchedSite), sites, units)
     // remember past states
     turns.add(battlefield)
 
-    println("WAIT")
-    println("TRAIN")
+    val commands = thinkVeryHard(turns, battlefield)
+
+    playTurn(commands.first, commands.second)
   }
+}
+
+fun thinkVeryHard(
+  turns: MutableList<Battlefield>,
+  battlefield: Battlefield
+): Pair<Action, Training> {
+  val queen = battlefield.queen
+
+  // take empty site if next to it
+  val action = if (battlefield.touchedSite.touches && battlefield.touchedSiteAsSite.isEmpty) {
+    Action.BuildKnightBarracks(battlefield.touchedSite.siteId)
+  } else {
+    // get the nearest unowned site
+    // TODO: travelling salesman problem?
+    val nearestSite = battlefield.emptySites.minBy {
+      it.position.distanceTo(queen.position)
+    }?.position
+
+    debug("Nearest empty site $nearestSite")
+
+    if (nearestSite != null) {
+      Action.Move(nearestSite)
+    } else {
+      // return to starting location for safety
+      Action.Move(turns.first().queen.position)
+    }
+  }
+
+  val build = if (battlefield.canBuildKnight && battlefield.iHaveBarracks) {
+    Training.AtLocation(battlefield.myBarracks.first().siteId)
+  } else {
+    Training.None
+  }
+
+  return action to build
+}
+
+fun parseSites(numSites: Int, input: Scanner): List<Site> {
+  val sites = (0 until numSites).map {
+    val siteId = input.nextInt()
+    val ignore1 = input.nextInt() // used in future leagues
+    val ignore2 = input.nextInt() // used in future leagues
+    val structureType = input.nextInt() // -1 = No structure, 2 = Barracks
+    val owner = input.nextInt() // -1 = No structure, 0 = Friendly, 1 = Enemy
+    val param1 = input.nextInt()
+    val param2 = input.nextInt()
+
+    Site(siteId, ignore1, ignore2, structureType, owner, param1, param2)
+  }
+  return sites
+}
+
+private fun parseUnits(
+  numUnits: Int,
+  input: Scanner
+): List<Soldier> {
+  val units = (0 until numUnits).map {
+    val x = input.nextInt()
+    val y = input.nextInt()
+    val owner = input.nextInt()
+    val unitType = input.nextInt() // -1 = QUEEN, 0 = KNIGHT, 1 = ARCHER
+    val health = input.nextInt()
+
+    Soldier(Position(x, y), owner, unitType, health)
+  }
+  return units
 }
--- app/src/test/kotlin/net/lab0/coderoyale/AiTest.kt
+++ app/src/test/kotlin/net/lab0/coderoyale/AiTest.kt
@@ -1,5 +1,17 @@
 package net.lab0.coderoyale
 
+import Action
+import Battlefield
+import EMPTY_STRUCTURE_TYPE
+import FRIENDLY_OWNER
+import MapSite
+import NO_OWNER
+import Position
+import QUEEN_TYPE
+import Site
+import Soldier
+import TouchedSite
+import Training
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import playTurn
@@ -8,7 +20,7 @@ internal class AiTest {
   @Test
   fun `can wait`() {
     // when
-    val out = playTurn(Action.Wait, TrainSoldiers.None)
+    val out = playTurn(Action.Wait, Training.None)
 
     // then
     assertThat(out[0]).isEqualTo("WAIT")
@@ -18,7 +30,7 @@ internal class AiTest {
   @Test
   fun `can move`() {
     // when
-    val out = playTurn(Action.Move(12, 116), TrainSoldiers.None)
+    val out = playTurn(Action.Move(12, 116), Training.None)
 
     // then
     assertThat(out[0]).isEqualTo("MOVE 12 116")
@@ -28,7 +40,7 @@ internal class AiTest {
   @Test
   fun `can build knights barracks`() {
     // when
-    val out = playTurn(Action.BuildKnightBarracks(116), TrainSoldiers.None)
+    val out = playTurn(Action.BuildKnightBarracks(116), Training.None)
 
     // then
     assertThat(out[0]).isEqualTo("BUILD 116 BARRACKS-KNIGHT")
@@ -38,7 +50,7 @@ internal class AiTest {
   @Test
   fun `can build archer barracks`() {
     // when
-    val out = playTurn(Action.BuildArcherBarracks(116), TrainSoldiers.None)
+    val out = playTurn(Action.BuildArcherBarracks(116), Training.None)
 
     // then
     assertThat(out[0]).isEqualTo("BUILD 116 BARRACKS-ARCHER")
@@ -48,7 +60,7 @@ internal class AiTest {
   @Test
   fun `can train at 3 sites`() {
     // when
-    val out = playTurn(Action.Wait, TrainSoldiers.AtLocation(1,10, 116))
+    val out = playTurn(Action.Wait, Training.AtLocation(1, 10, 116))
 
     // then
     assertThat(out[0]).isEqualTo("WAIT")
@@ -58,10 +70,29 @@ internal class AiTest {
   @Test
   fun `can do nothing`() {
     // when
-    val out = playTurn(Action.Wait, TrainSoldiers.None)
+    val out = playTurn(Action.Wait, Training.None)
 
     // then
     assertThat(out[0]).isEqualTo("WAIT")
     assertThat(out[1]).isEqualTo("TRAIN")
+  }
+
+  @Test
+  fun `can find the empty sites`() {
+    // given
+    val mapSite1 = MapSite(1, Position(0, 0), 5)
+    val mapSite2 = MapSite(2, Position(25, 25), 5)
+    val mapSites = listOf(mapSite1, mapSite2)
+    val sites = mapSites.map {
+      Site(it.siteId, 0, 0, EMPTY_STRUCTURE_TYPE, NO_OWNER, 0, 0)
+    }
+    val queen = Soldier(Position(5, 5), FRIENDLY_OWNER, QUEEN_TYPE, 100)
+    val battlefield = Battlefield(mapSites, 0, TouchedSite(0), sites, listOf(queen))
+
+    // when
+    val emptySites = battlefield.emptySites
+
+    // then
+    assertThat(emptySites).containsExactly(mapSite1, mapSite2)
   }
 }
